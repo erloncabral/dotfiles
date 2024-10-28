b9c4d2fec4384a5cbabe22b82017e3cd917f18ad
Erlon Cabral <erlon.cabral@gmail.com>
2024-10-28 09:24:59 -0300

    Add .config/zshrc/chezmoi.zsh

---
+++ dot_config/zshrc/chezmoi.zsh
@@ -0,0 +1,7 @@
+# https://www.chezmoi.io/
+alias chezedit="chezmoi edit --apply"
+
+alias i3config="chezedit $HOME/.config/i3/config"
+alias kitconfig="chezedit $HOME/.config/kitty/kitty.conf"
+alias piconfig="chezedit $HOME/.config/picom/picom.conf"
+alias polyconfig="chezedit $HOME/.config/polybar/config.ini"
