01a120f4c429671da8949814e6ecdc026e1a8912
Erlon Cabral <erlon.cabral@gmail.com>
2024-12-10 17:12:26 -0300

    Update .config/zshrc/chezmoi.zsh

--- dot_config/zshrc/chezmoi.zsh
+++ dot_config/zshrc/chezmoi.zsh
@@ -5,3 +5,5 @@ alias i3config="chezedit $HOME/.config/i3/config"
 alias kitconfig="chezedit $HOME/.config/kitty/kitty.conf"
 alias piconfig="chezedit $HOME/.config/picom/picom.conf"
 alias polyconfig="chezedit $HOME/.config/polybar/config.ini"
+
+alias zshconfig="chezedit $HOME/.zshrc"
